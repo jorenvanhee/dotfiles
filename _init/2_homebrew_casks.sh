58858d37e5edcff7c53fcafaa063a89bcd95b666
Joren Van Hee <j.van.hee@napoleongames.be>
2019-10-08 15:42:32 +0200

    Add postman

--- _init/2_homebrew_casks.sh
+++ _init/2_homebrew_casks.sh
@@ -9,6 +9,7 @@ brew cask install vlc
 brew cask install docker
 brew cask install sequel-pro
 brew cask install harvest
+brew cask install postman
 
 # Manually installed the nightly version because I wanted neovim 0.4.0 with
 # support for floating windows.
