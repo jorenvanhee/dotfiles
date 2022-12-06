e9c9108e73b4463eaeaf9461ae642fcf81caa54b
Joren Van Hee <joren.vanhee@catsolutions.be>
2022-12-06 11:05:03 +0100

    Add ruby and lua to treesitter config

--- dotfiles/nvim/after/plugin/nvim-treesitter.lua
+++ dotfiles/nvim/after/plugin/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "javascript", "php", "vue" },
+    ensure_installed = { "javascript", "php", "vue", "ruby", "lua" },
     highlight = {
         enable = true,
     },
