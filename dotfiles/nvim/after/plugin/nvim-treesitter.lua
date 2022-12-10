cdab1ac2b4947a94dcdfb5f7ea9908eaa15937a0
Joren Van Hee <231202+jorenvanhee@users.noreply.github.com>
2022-12-10 17:07:50 +0100

    Enable treesitter typescript and tsx

--- dotfiles/nvim/after/plugin/nvim-treesitter.lua
+++ dotfiles/nvim/after/plugin/nvim-treesitter.lua
@@ -1,6 +1,9 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "javascript", "php", "vue", "ruby", "lua", "vim" },
+    ensure_installed = { "javascript", "php", "vue", "ruby", "lua", "vim", "typescript", "tsx" },
     highlight = {
+        enable = true,
+    },
+    indent = {
         enable = true,
     },
 })
