27f8b900069dff52e0e03fe6358b2c3f9ed9cc8b
Joren Van Hee <231202+jorenvanhee@users.noreply.github.com>
2023-03-25 16:38:18 +0100

    Clean up packer file

--- dotfiles/nvim/lua/joren/packer.lua
+++ dotfiles/nvim/lua/joren/packer.lua
@@ -1,47 +1,67 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
     -- Packer can manage itself
     use("wbthomason/packer.nvim")
+
+    -- Color schemes
     use("folke/tokyonight.nvim")
+
+    -- Completion
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("onsails/lspkind.nvim")
+
+    -- File explorer
+    use("nvim-tree/nvim-tree.lua")
+    use("nvim-tree/nvim-web-devicons")
+
+    -- Syntax highlighting
+    use("lumiliet/vim-twig")
+    use("jwalton512/vim-blade")
+
+    -- LSP related
+    use("neovim/nvim-lspconfig")
+    use({
+        "jose-elias-alvarez/null-ls.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
+    })
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
+    use("jay-babu/mason-null-ls.nvim")
+    use("ray-x/lsp_signature.nvim")
+
+    -- Telescope
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        requires = { "nvim-lua/plenary.nvim" },
+    })
+    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+
+    -- Treesitter
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require("nvim-treesitter.install").update({
+                with_sync = true,
+            })
+
+            ts_update()
+        end,
+    })
+
+    -- Other
     use("justinmk/vim-sneak")
     use("tpope/vim-surround")
     use("tpope/vim-commentary")
     use("tpope/vim-repeat")
     use("tpope/vim-abolish")
-    use("nvim-tree/nvim-tree.lua")
     use("windwp/nvim-autopairs")
-    use({
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.0",
-        requires = { { "nvim-lua/plenary.nvim" } },
-    })
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("onsails/lspkind.nvim")
-    use("williamboman/mason.nvim")
-    use("williamboman/mason-lspconfig.nvim")
-    use("neovim/nvim-lspconfig")
-    use("lumiliet/vim-twig")
     use("gpanders/editorconfig.nvim")
     use("lewis6991/gitsigns.nvim")
     use("mattn/emmet-vim")
-    use("ray-x/lsp_signature.nvim")
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = function()
-            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-            ts_update()
-        end,
-    })
-    use("nvim-tree/nvim-web-devicons")
-    use("jwalton512/vim-blade")
-    use("jose-elias-alvarez/null-ls.nvim")
-    use("jay-babu/mason-null-ls.nvim")
     use("folke/which-key.nvim")
 end)
