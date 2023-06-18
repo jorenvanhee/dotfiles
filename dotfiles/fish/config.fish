783bf33c819a4c5ae486d525a274e77d3ed757d9
Joren Van Hee <231202+jorenvanhee@users.noreply.github.com>
2023-06-18 17:33:53 +0200

    Add more fish abbreviations

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,2 +1,9 @@
 # Setup zoxide
 zoxide init fish | source
+
+# Abbreviations
+abbr --add cat bat
+
+# Git abbreviations (next to the ones provided by jhillyerd/plugin-git)
+abbr --add gs git status -s
+abbr --add gdc git diff --cached
