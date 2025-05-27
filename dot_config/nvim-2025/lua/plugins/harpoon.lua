75a68e35dda534885688d6b1e846b0795a540050
Joren Van Hee <231202+jorenvanhee@users.noreply.github.com>
2025-05-27 22:00:30 +0200

    Update harpoon mappings to avoid collisions

--- dot_config/nvim-2025/lua/plugins/harpoon.lua
+++ dot_config/nvim-2025/lua/plugins/harpoon.lua
@@ -12,8 +12,8 @@ return {
       },
     })
 
-    vim.keymap.set('n', '<leader>ha', function() harpoon:list():add() end, { desc = 'Harpoon add file' })
-    vim.keymap.set('n', '<leader>hs', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon show list' })
+    vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = 'Harpoon add file' })
+    vim.keymap.set('n', '<space>h', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon show list' })
     vim.keymap.set('n', '<M-j>', function() harpoon:list():select(1) end, { desc = '' })
     vim.keymap.set('n', '<M-k>', function() harpoon:list():select(2) end, { desc = '' })
     vim.keymap.set('n', '<M-l>', function() harpoon:list():select(3) end, { desc = '' })
